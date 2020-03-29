2bc3a455513384ce2e3624b1635d4a0a913a5a0c
LauDela <laudela@gmail.com>
2020-03-29 17:46:37 +0200

    v0.1

--- web/lib.php
+++ web/lib.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-
+include_once("conf.php");
 date_default_timezone_set(get_ha_timezone());
 
 $logfile="/share/kodi2sonos/kodi2sonos.log";
